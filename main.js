51b243984a19184430de2f7cacc7b9616b013ff1
Welblaud <honza.hejzl@gmail.com>
2019-01-27 21:20:53 +0100

    Initial stage before using DOM

---
+++ main.js
@@ -0,0 +1,43 @@
+const quiz = [
+  { name: "Superman", realName: "Clark Kent" },
+  { name: "Wonder Woman", realName: "Diana Prince" },
+  { name: "Batman", realName: "Bruce Wayne" }
+];
+
+const game = {
+
+  start(quiz) {
+    this.questions = [...quiz];
+    this.score = 0; // initialize score
+  
+    // Iterating over questions, main game loop
+    for (const question of this.questions) {
+      this.question = question;
+      this.ask();
+    } // END of the main game loop 
+
+    this.gameOver();
+  },
+
+  ask() {
+    const question = `What is ${this.question.name}'s real name?`;
+    const response = prompt(question);
+    this.check(response);
+  },
+
+  check(response) {
+    const answer = this.question.realName;
+    if (response === answer) {
+      alert('Correct!');
+      this.score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}.`);
+    }
+  },
+
+  gameOver() {
+    alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}.`);
+  }
+}
+
+game.start(quiz);
